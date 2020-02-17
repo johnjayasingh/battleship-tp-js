27f768db72894c939550ac29b5647a4cf0c6b921
john <john>
2020-02-17 22:55:45 +0530

    day 1

---
+++ battleship_handler.js
@@ -0,0 +1,21 @@
+const { TransactionHandler } = require('sawtooth-sdk/processor/handler');
+const crypto = require('crypto');
+
+const TP_FAMILY = 'battleship';
+const TP_NAMESPACE = '010101';
+const TP_VERSION = '1.0';
+
+class BattleShipTransactionHandler extends TransactionHandler {
+    constructor() {
+        super(TP_FAMILY, [TP_VERSION], [TP_NAMESPACE])
+    }
+
+    apply(transaction, context) {
+        return new Promise((resolve, reject) => {
+
+        })
+    }
+
+}
+
+module.exports = BattleShipTransactionHandler;
