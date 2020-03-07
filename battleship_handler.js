f3ec50cf1431788a636bbaa407a97029aa92152e
john <johnjayasingh.s@gmail.com>
2020-03-08 02:24:06 +0530

    1.1

--- battleship_handler.js
+++ battleship_handler.js
@@ -1,18 +1,36 @@
 const { TransactionHandler } = require('sawtooth-sdk/processor/handler');
 const crypto = require('crypto');
 
+const _hash = (x) =>
+    crypto.createHash('sha512').update(x).digest('hex').toLowerCase()
+
 const TP_FAMILY = 'battleship';
-const TP_NAMESPACE = '010101';
-const TP_VERSION = '1.0';
+const TP_NAMESPACE = _hash(TP_FAMILY).substr(0, 6);
+const TP_VERSION = '1.1';
 
 class BattleShipTransactionHandler extends TransactionHandler {
     constructor() {
         super(TP_FAMILY, [TP_VERSION], [TP_NAMESPACE])
     }
 
+    /**
+     * Return promise with state change as result or throws error
+     * @param {*} transaction 
+     * @param {*} context 
+     */
     apply(transaction, context) {
-        return new Promise((resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
+            let address = TP_NAMESPACE + _hash('sampleKey').substr(0, 64)
 
+            let previousValue = await context.getState([address]);
+
+
+
+            let entries = {
+                [address]: JSON.stringify('1')
+            }
+            return context.setState(entries)
+            // never throw InternalError as this code will run forever and attempt to commit the changes forever too
         })
     }
 
---
+++ server.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/**
+ * Need to be modulirized later 
+ * Sigining and forwarding transaction
+ */
+
+const API_URL = 'http://localhost:8008'
+
+
+const { createContext, CryptoFactory } = require('sawtooth-sdk/signing')
+const { createHash } = require('crypto')
+const crypto = require('crypto')
+const { protobuf } = require('sawtooth-sdk')
+const axios = require('axios').default;
+
+const context = createContext('secp256k1')
+const privateKey = context.newRandomPrivateKey()
+const signer = new CryptoFactory(context).newSigner(privateKey)
+
+
+const _hash = (x) =>
+    crypto.createHash('sha512').update(x).digest('hex').toLowerCase()
+
+
+const TP_FAMILY = 'battleship';
+const TP_NAMESPACE = _hash(TP_FAMILY).substr(0, 6);
+const TP_VERSION = '1.1';
+
+
+(async () => {
+    try {
+
+
+        let address = TP_NAMESPACE + _hash('sampleKey').substr(0, 64)
+
+        console.log({
+            TP_NAMESPACE,
+            address
+        })
+
+        const payload = {
+            Name: 'Does not matter as we have not implemented any check'
+        }
+
+        // Input for one transaction
+        const payloadBytes = Buffer.from(JSON.stringify(payload))
+
+        // Output we created with this transaction input
+
+        const transactionHeaderBytes = protobuf.TransactionHeader.encode({
+            familyName: TP_FAMILY,
+            familyVersion: TP_VERSION,
+            // Needs to be same as the expected address we create in contract
+            // If diffrent we wont get access to put state and get state of the address
+            inputs: [address],
+            outputs: [address],
+            signerPublicKey: signer.getPublicKey().asHex(),
+            batcherPublicKey: signer.getPublicKey().asHex(),
+            dependencies: [],
+            payloadSha512: createHash('sha512').update(payloadBytes).digest('hex')
+        }).finish()
+
+        const signature = signer.sign(transactionHeaderBytes)
+
+        // Sign the transaction
+        const transaction = protobuf.Transaction.create({
+            header: transactionHeaderBytes,
+            headerSignature: signature,
+            payload: payloadBytes
+        })
+
+        // Wrap it into list of transaction
+        const transactions = [transaction]
+
+        const batchHeaderBytes = protobuf.BatchHeader.encode({
+            signerPublicKey: signer.getPublicKey().asHex(),
+            transactionIds: transactions.map((txn) => txn.headerSignature),
+        }).finish()
+
+        // Wrap the transaction list into batch
+        const batchSignature = signer.sign(batchHeaderBytes)
+
+        // And sign it
+        const batch = protobuf.Batch.create({
+            header: batchHeaderBytes,
+            headerSignature: batchSignature,
+            transactions: transactions
+        });
+
+        // Wrap them in batch list
+        const batchListBytes = protobuf.BatchList.encode({
+            batches: [batch]
+        }).finish()
+        const response = await axios.post(`${API_URL}/batches`, batchListBytes, {
+            headers: { 'Content-Type': 'application/octet-stream' }
+        })
+       console.log(response)
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+app.get('/test', async (req, res) => {
+
+})
+
+app.listen(8080, () => console.log('Server started'));
